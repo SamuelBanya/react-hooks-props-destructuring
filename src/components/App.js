b7b7c9dbd6c5a2acabcc265ba0a45df658727768
Samuel Banya <sbanya@fastmail.com>
2022-06-25 10:33:14 -0400

    Adding personal revisions to include The Wizard Of Oz

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,14 @@ function App() {
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
+  const newTitle = "The Wizard Of Oz";
+  const newPosterURL = "https://upload.wikimedia.org/wikipedia/commons/6/69/Wizard_of_oz_movie_poster.jpg";
+  const newGenresArr = ["Adventure", "Family", "Fantasy"];
+
   return (
     <div className="App">
       <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={newTitle} posterSrc={newPosterURL} genres={newGenresArr}/>
     </div>
   );
 }
